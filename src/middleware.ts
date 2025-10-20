f47e30bdaa662367fbd989cc3704c8de2769ca08
chriscampbellt <christopher.campbell.t@gmail.com>
2025-10-20 20:30:28 +0500

    feat: fix the middleware path correction

--- src/middleware.ts
+++ src/middleware.ts
@@ -31,7 +31,7 @@ export function middleware(req: NextRequest) {
 
 export const config = {
   matcher: [
-    '/:path*',
+    '/admin/:path*',
     '/((?!_next/static|_next/image|favicon.ico|.*\\.png$|.*\\.jpg$).*)',
   ],
 };
